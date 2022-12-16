75eeb934aef5d6b9cbac0c8ce9412738cd8f6bbb
Nonso Paul <101235161+CAv100@users.noreply.github.com>
2022-12-16 06:54:04 +0100

    Update 12.m

--- bc/12.m
+++ bc/12.m
@@ -1,10 +1,17 @@
-#pchar
-push 36
-pall
-pchar
-push 127
-pall
-pchar
-push 10
-pall
-pchar
+push 1
+push 2
+push 3
+push 4
+push 0
+push 110
+push 00
+push 110
+push 111
+push 116
+push 114
+push 101
+push 98
+push 108
+push 111
+push 72
+pstr
